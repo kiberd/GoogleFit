2df352f9f047bc17cc5f2513a4fc52c0bd344777
kiberd <kiberd22@gmail.com>
2020-05-19 11:23:57 +0900

    평균계산

--- app/src/main/java/com/example/googlefit/MainActivity.java
+++ app/src/main/java/com/example/googlefit/MainActivity.java
@@ -71,15 +71,24 @@ import static java.text.DateFormat.getTimeInstance;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
+    // 시작 끝 시간 임의 설정 2019년 5월 19일 수면기록
+    private String startTime = "20200519004000"; // start 타임
+    private String endTime = "20200519073200"; // end 타임
+
+    private static final int ACC = 0;
+
     private static final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = 1;
-    private static final int MY_PERMISSIONS_ACTIVITY_RECOGNITION = 1;
     private FitnessOptions fitnessOptions;
 
-    private OnDataPointListener mListener;
+    // 최근 7일 동안의 평균 심박수 및 최저 심박수
+    private float avg_bpm = 0;
+    private float min_bpm = 0;
 
+    // 0 = 기상, 1 = 얕은수면, 2 = 깊은수면
+    private int sleepStage[] = new int [3];
 
     TextView as, bs, cs;
+
 
 
     @Override
@@ -87,20 +96,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Case of Android 10
         requestActivityPermission();
 
         // 피트니스 객체 지정
         fitnessOptions = FitnessOptions.builder()
-                //.addDataType(DataType.TYPE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                //.addDataType(DataType.AGGREGATE_STEP_COUNT_DELTA, FitnessOptions.ACCESS_READ)
-                //.addDataType(DataType.TYPE_ACTIVITY_SEGMENT, fitnessOptions.ACCESS_READ)
                 .addDataType(DataType.TYPE_HEART_RATE_BPM, fitnessOptions.ACCESS_READ)
                 .addDataType(DataType.TYPE_HEART_RATE_BPM, fitnessOptions.ACCESS_WRITE)
-                .addDataType(DataType.TYPE_MOVE_MINUTES, fitnessOptions.ACCESS_WRITE)
-                .addDataType(DataType.TYPE_MOVE_MINUTES, fitnessOptions.ACCESS_WRITE)
                 .build();
 
         GoogleSignInAccount account = GoogleSignIn.getAccountForExtension(this, fitnessOptions);
+
 
         if (!GoogleSignIn.hasPermissions(account, fitnessOptions)) {
             GoogleSignIn.requestPermissions(
@@ -110,14 +116,26 @@ public class MainActivity extends AppCompatActivity {
                     fitnessOptions);
         } else {
 
-            accessHeartLate();
-
+            setHeartLate(); // 최근 7일동안 평균심박수, 최저심박수 구하는 method
+            setSleepStage(); // 수면단계 구분 method
         }
-
-
     }
 
-    // ACTIVITY READ 권한 요청
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+
+                setHeartLate();// 최근 7일동안 평균심박수, 최저심박수 구하는 method
+                setSleepStage(); // 수면단계 구분 method
+            }
+        }
+    }
+
+
+    // ACTIVITY READ 권한 요청 (Case of Android 10)
     private void requestActivityPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACTIVITY_RECOGNITION)
                 != PackageManager.PERMISSION_GRANTED) {
@@ -129,79 +147,146 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == GOOGLE_FIT_PERMISSIONS_REQUEST_CODE) {
+    // 수면단계 결정 ( getTimeInMillis() 형식 스타트,엔드 )
+    private void setSleepStage(){
 
-                accessHeartLate();
+        // Range : 지난 7일동안
+        Calendar start = Calendar.getInstance();
+        start.set(Calendar.YEAR, 2019);
+        start.set(Calendar.MONTH, Calendar.MAY );
+        start.set(Calendar.DATE, 19);
 
-            }
-        }
-    }
+        start.set(Calendar.HOUR_OF_DAY, 00);
+        start.set(Calendar.MINUTE, 40);
+        start.set(Calendar.SECOND, 00);
 
-
-    private void accessHeartLate() {
-
-
-        // 출력을 위한 어레이리스트
-        final ArrayList<String> heartLateList = new ArrayList<>();
-
-        // 하루 동안의 심박수
-        Calendar cal = Calendar.getInstance();
-        Date now = new Date();
-        cal.setTime(now);
-        final long endTime = cal.getTimeInMillis();
-        cal.add(Calendar.DAY_OF_YEAR, -1);
-        final long startTime = cal.getTimeInMillis();
+        final long startTime = start.getTimeInMillis();
 
 
+        Calendar end = Calendar.getInstance();
+        end.set(Calendar.YEAR, 2019);
+        end.set(Calendar.MONTH, Calendar.MAY );
+        end.set(Calendar.DATE, 19);
+
+        end.set(Calendar.HOUR_OF_DAY, 07);
+        end.set(Calendar.MINUTE, 32);
+        end.set(Calendar.SECOND, 00);
+
+        final long endTime = end.getTimeInMillis();
+
+        // 심박수 데이터 getHistoryClient (2020년 5월 19일 수면기록 : 00시 40분 ~ 07시 32분)
         Fitness.getHistoryClient(this,
                 GoogleSignIn.getLastSignedInAccount(this))
                 .readData(new DataReadRequest.Builder()
-                        //.read(DataType.TYPE_STEP_COUNT_DELTA) // Raw 걸음 수
                         .read(DataType.TYPE_HEART_RATE_BPM)// 심박수
                         .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                         .build())
                 .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
                     @Override
                     public void onSuccess(DataReadResponse response) {
-                        DataSet dataSet = response.getDataSet(DataType.TYPE_HEART_RATE_BPM);
-                        Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
+
+                        float G = avg_bpm - min_bpm; // 평상시 심박수 평균 - 최저 심박수 (최근 7일동안)
+                        float S; // 현재 심박수 - 최저 심박수 (최근 7일동안)
 
 
+                        SimpleDateFormat format = new SimpleDateFormat("d일:h시:mm분");
                         bs = findViewById(R.id.b);
+                        bs.setText(format.format(endTime - startTime) + " " + format.format(endTime));
 
-                        Log.i(TAG, "Data returned for Data type: " + dataSet.getDataPoints());
+
+                        DataSet dataSet = response.getDataSet(DataType.TYPE_HEART_RATE_BPM);
 
                         for (DataPoint dp : dataSet.getDataPoints()) {
 
-                            SimpleDateFormat format = new SimpleDateFormat("a h:mm");
-
-                            Log.i(TAG, "Data point:");
-                            Log.i(TAG, "\tType: " + dp.getDataType().getName());
-                            Log.i(TAG, "\tEnd: " + (dp.getEndTime(TimeUnit.MILLISECONDS)));
-
-
                             for (Field field : dp.getDataType().getFields()) {
 
-                                heartLateList.add("\tTime: " + format.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " Value: " + dp.getValue(field));
+                                S = Float.valueOf(String.valueOf(dp.getValue(field))) - min_bpm;
 
-                                Log.i(TAG, "\tField: " + field.getName() + " Value: " + dp.getValue(field));
-                                bs.setText("\t가장 최근 동기화된 시각: " + format.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " Value: " + dp.getValue(field));
+                                if(S > (0.5 * G)){ // 기상 상태
+                                    sleepStage[0] += 1;
+                                }
+                                else{
+
+                                        if(S < (0.2 * G)){ // 깊은 수면
+                                            sleepStage[2] += 1;
+                                        }
+                                        else{ // 얕은 수면
+                                            sleepStage[1] += 1;
+                                        }
+                                    }
+
+                                }
                             }
-
-                        }
+                        cs = findViewById(R.id.c);
+                        cs.setText("기상 : " + sleepStage[0] +", 얕은수면 : " + sleepStage[1] + ", 깊은수면 : " + sleepStage[2]);
                     }
                 });
 
+
+    }
+
+
+    // 최근 7일동안 평균심박수 및 최저심박수 설정
+    private void setHeartLate() {
+
+        // 출력을 위한 어레이리스트
+        final ArrayList<String> heartLateList = new ArrayList<>();
+
+        // Range : 지난 7일동안
+        Calendar cal = Calendar.getInstance();
+        Date now = new Date();
+        cal.setTime(now);
+        final long endTime = cal.getTimeInMillis();
+        cal.add(Calendar.WEEK_OF_YEAR, -1);
+        final long startTime = cal.getTimeInMillis();
+
+
+        // 심박수 데이터 getHistoryClient
+        Fitness.getHistoryClient(this,
+                GoogleSignIn.getLastSignedInAccount(this))
+                .readData(new DataReadRequest.Builder()
+                        .read(DataType.TYPE_HEART_RATE_BPM)// 심박수
+                        .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
+                        .build())
+                .addOnSuccessListener(new OnSuccessListener<DataReadResponse>() {
+                    @Override
+                    public void onSuccess(DataReadResponse response) {
+
+                        as = findViewById(R.id.a);
+
+                        float count = 0, total = 0;
+                        DataSet dataSet = response.getDataSet(DataType.TYPE_HEART_RATE_BPM);
+
+                        for (DataPoint dp : dataSet.getDataPoints()) {
+
+                            SimpleDateFormat format = new SimpleDateFormat("d일:h시:mm분");
+                            for (Field field : dp.getDataType().getFields()) {
+
+                                // 첫번째 값일떄
+                                if(count ==0){
+                                    min_bpm = Float.valueOf(String.valueOf(dp.getValue(field)));
+                                }
+                                else{
+                                    if(Float.valueOf(String.valueOf(dp.getValue(field))) < min_bpm){
+                                        min_bpm = Float.valueOf(String.valueOf(dp.getValue(field)));
+                                    }
+                                }
+                                count++;
+                                total += Float.valueOf(String.valueOf(dp.getValue(field)));
+
+                                heartLateList.add(format.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " Value: " + dp.getValue(field));
+                                //bs.setText("\t가장 최근 동기화된 시각: " + format.format(dp.getEndTime(TimeUnit.MILLISECONDS)) + " Value: " + dp.getValue(field));
+                            }
+                            avg_bpm = total / count;
+                            as.setText("data수 : " + count + ", AVG : " + Math.round(avg_bpm) + ", MIN : " + Math.round(min_bpm));
+                        }
+                    }
+                });
 
         ListView printView = (ListView)findViewById(R.id.bpm);
         final ArrayAdapter<String> timeAdabtor = new ArrayAdapter<String>(
                 this, android.R.layout.simple_list_item_1, heartLateList);
         printView.setAdapter(timeAdabtor);
-
 
     }
 }
